a719ee1c56d3e4e9f147a265dc6640b15c89c454
hibarahmaoui <79206510+hibarahmaoui@users.noreply.github.com>
2023-08-08 23:04:04 +0100

    Update AddCourse.java

--- CoursesPlus-2/src/main/java/AddCourse.java
+++ CoursesPlus-2/src/main/java/AddCourse.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Mohamad
- */
 import java.sql.*; 
 public class AddCourse extends javax.swing.JFrame {
     String url = "jdbc:postgresql://localhost/mortajim"; 
